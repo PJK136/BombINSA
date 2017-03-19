a06e40cc23601f680ca6d086cfbab16fcffec81d
jeannib <bast.jeannin@gmail.com>
2017-03-19 16:23:34 +0100

    rééquilibrage lootRate

--- Java/src/game/BonusType.java
+++ Java/src/game/BonusType.java
@@ -5,9 +5,9 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 @objid ("7787b9db-abdc-433d-863e-9ffee325df12")
 public enum BonusType {
     Random((3./5.)*(1./10.)),
-    MoreBomb((3./5.)*(1./10.)),
+    MoreBomb((3./5.)*(1./5.)),
     LessBomb((2./5.)*(1./3.)),
-    MoreRange((3./5.)*(3./10.)),
+    MoreRange((3./5.)*(1./5.)),
     LessRange((2./5.)*(1./3.)),
     MoreSpeed((3./5.)*(1./5.)),
     LessSpeed((2./5.)*(1./3.)),
