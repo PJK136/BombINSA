fc6308a1306846234ac72e34a375f9119ffba1b3
jeannib <bast.jeannin@gmail.com>
2017-03-21 13:36:45 +0100

    rééquilibrage final des bonus

--- Java/src/game/BonusType.java
+++ Java/src/game/BonusType.java
@@ -4,15 +4,15 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("7787b9db-abdc-433d-863e-9ffee325df12")
 public enum BonusType {
-    Random((3./5.)*(1./10.)),
-    MoreBomb((3./5.)*(1./5.)),
-    LessBomb((2./5.)*(1./3.)),
-    MoreRange((3./5.)*(1./5.)),
-    LessRange((2./5.)*(1./3.)),
-    MoreSpeed((3./5.)*(1./5.)),
-    LessSpeed((2./5.)*(1./3.)),
-    Shield((3./5.)*(1./10.)),
-    Kick((3./5.)*(1./5.));
+    Random((3./4.)*(1./10.)),
+    MoreBomb((3./4.)*(4./10.)),
+    LessBomb((1./4.)*(1./3.)),
+    MoreRange((3./4.)*(1./10.)),
+    LessRange((1./4.)*(1./3.)),
+    MoreSpeed((3./4.)*(1./10.)),
+    LessSpeed((1./4.)*(1./3.)),
+    Shield((3./5.)*(1./5.)),
+    Kick((3./5.)*(1./10.));
     private double lootRate;
     
     private BonusType(double lr) {
