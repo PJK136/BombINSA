2c48324a79a69de441d441369a562132c3836e98
Paul Du <du.paul136@gmail.com>
2017-04-04 23:37:24 +0200

    Corrections mineures et ajout de maps

--- Java/src/gui/GameType.java
+++ Java/src/gui/GameType.java
@@ -6,7 +6,7 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 @objid ("f5a7d15e-110e-4488-a6f6-af5012d84a04")
 /**
  *   - En local (sur une seule machine, doit forcément jouer contre quelqu'un, IA ou autre joueur)
- *   - En L.A.N. : Server héberge la partie ; Client la rejoind
+ *   - En L.A.N. : Server héberge la partie ; Client la rejoint
  *   - SandBox : permet de jouer tout seul sur une carte (sans adversaires ni d'IA) 
  */
 public enum GameType {
